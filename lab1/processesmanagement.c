7c13d0309d641c4e54c79b9fe3fa7669cb213400
anb0037 <anb0037@auburn.edu>
2018-06-10 19:13:39 -0500

    adding debug statements

--- lab1/processesmanagement.c
+++ lab1/processesmanagement.c
@@ -164,8 +164,7 @@ void CPUScheduler(Identifier whichPolicy) {
     case RR   : selectedProcess = RR_Scheduler();
   }
   if (selectedProcess) {
-    selectedProcess->TimeInReadyQueue = Now() - selectedProcess->JobStartTime;
-    NumberofJobs[WT]++;
+    selectedProcess->TimeInReadyQueue += Now() - selectedProcess->JobStartTime;
     selectedProcess->state = RUNNING; // Process state becomes Running
     EnqueueProcess(RUNNINGQUEUE, selectedProcess); // Put process in Running Queue
     QueueLength[RUNNINGQUEUE]++;
@@ -257,7 +256,9 @@ void Dispatcher() {
   ProcessControlBlock *pcb = Queues[RUNNINGQUEUE].Tail;
   if (pcb != NULL) { 
     if (pcb->TimeInCpu >= pcb->TotalJobDuration) { // handles processes that have completed
+	    
 	    pcb->JobExitTime = Now();
+	    pcb->state = DONE;
 	    NumberofJobs[THGT]++;
 	    NumberofJobs[TAT]++;
         DequeueProcess(RUNNINGQUEUE);
@@ -265,13 +266,15 @@ void Dispatcher() {
         EnqueueProcess(EXITQUEUE, pcb);
         QueueLength[EXITQUEUE]++;
     }
-    else if (PolicyNumber == 3 && pcb->TimeInCpu >= pcb->CpuBurstTime) { // handles RR processes that aren't complete
+    else if (PolicyNumber == 3) { // handles RR processes that aren't complete
+        printf("kicking off pid %d after quantum %f total time: %f\n",pcb->ProcessID, pcb->CpuBurstTime, pcb->TimeInCpu); 
         DequeueProcess(RUNNINGQUEUE);
         EnqueueProcess(READYQUEUE, pcb);
         QueueLength[RUNNINGQUEUE]--;
         QueueLength[READYQUEUE]++;
     }
     else { // handles processes that still need computation
+    	printf("adding %f to pid %d\n", pcb->CpuBurstTime, pcb->ProcessID);
     	if (pcb->TimeInCpu == 0) {
     		pcb->StartCpuTime = Now();
     		NumberofJobs[RT]++;
