8a10c77ac635b8aafa79be9ea9d9e22f35a0c663
Adam N Brown <anb0037@auburn.edu>
2018-06-12 12:50:38 -0500

    adding data collection script

---
+++ lab2/script.sh
@@ -0,0 +1,9 @@
+mkdir out
+echo "executing pm with arg 1\n"
+./pm 1 > out/out1 
+echo "executing pm with arg 2\n"
+./pm 2 > out/out2
+echo "executing pm with arg 3\n"
+./pm 3 > out/out3
+echo "complete"
+exit
