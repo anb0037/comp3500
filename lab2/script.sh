bf60ced0b8f2590e1ce31489881db9397c80849e
anb0037 <anb0037@auburn.edu>
2018-06-12 15:36:46 -0500

    adding all runs to data collection script

--- lab2/script.sh
+++ lab2/script.sh
@@ -1,8 +1,47 @@
+rmdir -rf out
 mkdir out
-echo "executing pm with arg 1"
+echo "executing pm (FCFS)"
 ./pm 1 > out/out1 
-echo "executing pm with arg 2"
-./pm 2 > out/out2
-echo "executing pm with arg 3"
-./pm 3 > out/out3
+echo "grabbing tail of output"
+tail out/out1 -n 4 >> out/complete
+echo "\n" >> out/complete
+
+echo "executing pm (SRTF)"
+./pm 2 > out/out2 
+echo "grabbing tail of output"
+tail out/out12-n 4 >> out/complete
+echo "\n" >> out/complete
+
+
+echo "executing pm (RR) quantum 10ms"
+./pm 3 10 > out/out3
+echo "grabbing tail of output"
+tail out/out3 -n 4 >> out/complete
+echo "\n" >> out/complete
+
+
+echo "executing pm (RR) quantum 20ms"
+./pm 3 20 > out/out3
+echo "grabbing tail of output"
+tail out/out3 -n 4 >> out/complete
+echo "\n" >> out/complete
+
+echo "executing pm (RR) quantum 50ms"
+./pm 3 50 > out/out3
+echo "grabbing tail of output"
+tail out/out3 -n 4 >> out/complete
+echo "\n" >> out/complete
+
+echo "executing pm (RR) quantum 250ms"
+./pm 3 250 > out/out3
+echo "grabbing tail of output"
+tail out/out3 -n 4 >> out/complete
+echo "\n" >> out/complete
+
+echo "executing pm (RR) quantum 500ms"
+./pm 3 500 > out/out3
+echo "grabbing tail of output"
+tail out/out3 -n 4 >> out/complete
+echo "\n" >> out/complete
+
 echo "complete"
