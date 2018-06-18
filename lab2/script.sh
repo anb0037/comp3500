362cc5737a7d7488ccfd125259c701b5521f5a55
anb0037 <anb0037@auburn.edu>
2018-06-17 19:45:31 -0500

    modifying data collection script

--- lab2/script.sh
+++ lab2/script.sh
@@ -2,46 +2,30 @@ rmdir -rf out
 mkdir out
 echo "executing pm (FCFS)"
 ./pm 1 > out/out1 
-echo "grabbing tail of output"
-tail out/out1 -n 4 >> out/complete
-echo "\n" >> out/complete
+tail out/out1 -n 5 >> out/complete
 
 echo "executing pm (SRTF)"
 ./pm 2 > out/out2 
-echo "grabbing tail of output"
-tail out/out12-n 4 >> out/complete
-echo "\n" >> out/complete
-
+tail out/out2-n 5 >> out/complete
 
 echo "executing pm (RR) quantum 10ms"
 ./pm 3 10 > out/out3
-echo "grabbing tail of output"
-tail out/out3 -n 4 >> out/complete
-echo "\n" >> out/complete
-
+tail out/out3 -n 5 >> out/complete
 
 echo "executing pm (RR) quantum 20ms"
-./pm 3 20 > out/out3
-echo "grabbing tail of output"
-tail out/out3 -n 4 >> out/complete
-echo "\n" >> out/complete
+./pm 3 20 > out/out4
+tail out/out4 -n 5 >> out/complete
 
 echo "executing pm (RR) quantum 50ms"
-./pm 3 50 > out/out3
-echo "grabbing tail of output"
-tail out/out3 -n 4 >> out/complete
-echo "\n" >> out/complete
+./pm 3 50 > out/out5
+tail out/out5 -n 5 >> out/complete
 
 echo "executing pm (RR) quantum 250ms"
-./pm 3 250 > out/out3
-echo "grabbing tail of output"
-tail out/out3 -n 4 >> out/complete
-echo "\n" >> out/complete
+./pm 3 250 > out/out6
+tail out/out6 -n 5 >> out/complete
 
 echo "executing pm (RR) quantum 500ms"
-./pm 3 500 > out/out3
-echo "grabbing tail of output"
-tail out/out3 -n 4 >> out/complete
-echo "\n" >> out/complete
+./pm 3 500 > out/out7
+tail out/out7 -n 5 >> out/complete
 
 echo "complete"
